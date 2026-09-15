bf13f75c153963c7c34cf55184ce46ee5c8ab37f
Yannis Ress Lasser <yan.br@icloud.com>
2026-09-16 01:01:38 +0200

    Style XML sitemap for readable browser presentation

--- scripts/sitemap.mjs
+++ scripts/sitemap.mjs
@@ -5,5 +5,5 @@ export function sitemapXml() {
     const alternates = languageAlternates(path).map(item => `    <xhtml:link rel="alternate" hreflang="${item.lang}" href="${canonicalUrlFor(item.path)}" />`).join('\n')
     return `  <url>\n    <loc>${canonicalUrlFor(path)}</loc>\n${alternates}\n  </url>`
   })
-  return `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">\n${urls.join('\n')}\n</urlset>\n`
+  return `<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/css" href="/sitemap.css"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">\n${urls.join('\n')}\n</urlset>\n`
 }
